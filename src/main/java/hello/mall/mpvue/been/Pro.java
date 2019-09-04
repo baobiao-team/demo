631152fc942514eb1b2ec739d6bab78decda5cb4
albertBaigl <albert_baigl@163.com>
2019-09-04 10:25:15 +0800

    json

---
+++ src/main/java/hello/mall/mpvue/been/Pro.java
@@ -0,0 +1,64 @@
+package hello.mall.mpvue.been;
+
+public class Pro {
+				
+	public String proId;
+	public String proName;
+	public String proType;
+	public String proPrice;
+	public String proAddress;
+	public String proNum;
+	public String proDisc;
+	public String proImg;
+	public String getProId() {
+		return proId;
+	}
+	public void setProId(String proId) {
+		this.proId = proId;
+	}
+	public String getProName() {
+		return proName;
+	}
+	public void setProName(String proName) {
+		this.proName = proName;
+	}
+	public String getProType() {
+		return proType;
+	}
+	public void setProType(String proType) {
+		this.proType = proType;
+	}
+	public String getProPrice() {
+		return proPrice;
+	}
+	public void setProPrice(String proPrice) {
+		this.proPrice = proPrice;
+	}
+	public String getProAddress() {
+		return proAddress;
+	}
+	public void setProAddress(String proAddress) {
+		this.proAddress = proAddress;
+	}
+	public String getProNum() {
+		return proNum;
+	}
+	public void setProNum(String proNum) {
+		this.proNum = proNum;
+	}
+	public String getProDisc() {
+		return proDisc;
+	}
+	public void setProDisc(String proDisc) {
+		this.proDisc = proDisc;
+	}
+	public String getProImg() {
+		return proImg;
+	}
+	public void setProImg(String proImg) {
+		this.proImg = proImg;
+	}
+
+	
+	
+}
